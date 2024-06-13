44262edd08554db4c685c027fadc9ade8240228b
swadhi03 <swathiksanthosh03@gmail.com>
2024-06-14 02:32:49 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddBus from './components/AddBus';
 import SearchBus from './components/SearchBus';
+import ViewBus from './components/ViewBus';
 
 function App() {
   return (
     <div>
       <AddBus/>
       <SearchBus/>
+      <ViewBus/>
     </div>
   );
 }
