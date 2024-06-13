8e43ee259fe7d313311467a705f61bc91dbf6acc
swadhi03 <swathiksanthosh03@gmail.com>
2024-06-14 02:25:42 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddBus from './components/AddBus';
+import SearchBus from './components/SearchBus';
 
 function App() {
   return (
     <div>
       <AddBus/>
+      <SearchBus/>
     </div>
   );
 }
